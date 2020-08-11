16f4ccbdcce4249518138722e565d87ea535e62a
clokyy <64561638+clokyy@users.noreply.github.com>
2020-08-11 01:32:11 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const token = "NzQwMzI2MDg2MTk3NTEwMjA2.XynYZQ.8-L9nEAPGfuKyZ7sP5-UMpId-iw";
+const token = "YOUR_BOT_TOKEN";
 
 const PREFIX = "!"
 
